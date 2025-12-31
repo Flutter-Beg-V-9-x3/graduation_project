bdc1d5c447a6d813c60b7f31597441fb51938310
hala <halaa.mayhoob777@gmail.com>
2025-12-31 16:02:09 +0300

    new bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_graduation_project/core/theme/app_theme.dart';
+import 'core/theme/app_theme.dart';
+import 'features/home/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Beg',
-      theme:  AppTheme.theme,
-      home: const Placeholder(),
+      title: 'Moive',
+      theme: AppTheme.theme,
+      home: const HomeScreen(),
     );
   }
 }
-
