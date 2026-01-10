d67d2ad61c3a6e377f06caac2dc40bf01d7ba0fd
ahmadalabdi29-dev <ahmadalabdi29@gmail.com>
2026-01-10 05:18:08 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Beg',
       theme: AppTheme.theme,
-      home: MovieDetailsScreen(),
+      home: MainNavigation(),
     );
   }
 }
