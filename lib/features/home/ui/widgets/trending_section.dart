869a7a06461e40c31daee2307e60a026391adf4a
ahmadalabdi29-dev <ahmadalabdi29@gmail.com>
2026-01-16 22:39:02 +0300

    yousef fix TrendingSection

--- lib/features/home/ui/widgets/trending_section.dart
+++ lib/features/home/ui/widgets/trending_section.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_graduation_project/core/navigation/navigation_manager.dart';
 import 'package:flutter_graduation_project/core/theme/app_text_styles.dart';
@@ -14,7 +15,11 @@ class TrendingSection extends StatelessWidget {
     if (movies.isEmpty) return const SizedBox.shrink();
 
     final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     final isSmallScreen = screenWidth < 600;
+
+    final sectionHeight = (screenHeight * 0.35).clamp(320.0, 560.0);
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -30,7 +35,7 @@ class TrendingSection extends StatelessWidget {
               Text(
                 "Trending Now",
                 style: AppTextStyles.title.copyWith(
-                  fontSize: isSmallScreen ? 20 : 24,
+                  fontSize: isSmallScreen ? 20 : 26,
                 ),
               ),
               TextButton(
@@ -49,15 +54,22 @@ class TrendingSection extends StatelessWidget {
         ),
 
         SizedBox(
-          height: screenWidth * 0.75,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            padding: EdgeInsets.only(left: isSmallScreen ? 16 : 24),
-            physics: const BouncingScrollPhysics(),
-            itemCount: movies.length,
-            itemBuilder: (context, index) {
-              return TrendingMovieCard(movie: movies[index]);
-            },
+          height: sectionHeight,
+          child: ScrollConfiguration(
+            behavior: const MaterialScrollBehavior().copyWith(
+              dragDevices: {PointerDeviceKind.touch, PointerDeviceKind.mouse},
+            ),
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              physics: const BouncingScrollPhysics(),
+              padding: EdgeInsets.symmetric(
+                horizontal: isSmallScreen ? 16 : 24,
+              ),
+              itemCount: movies.length,
+              itemBuilder: (context, index) {
+                return TrendingMovieCard(movie: movies[index]);
+              },
+            ),
           ),
         ),
       ],
@@ -75,98 +87,107 @@ class TrendingMovieCard extends StatelessWidget {
     final screenWidth = MediaQuery.of(context).size.width;
     final isSmallScreen = screenWidth < 600;
 
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        final cardWidth = screenWidth * (isSmallScreen ? 0.45 : 0.38);
+    final cardWidth = (screenWidth * (isSmallScreen ? 0.45 : 0.30)).clamp(
+      160.0,
+      320.0,
+    );
 
-        return GestureDetector(
-          onTap: () {
-            NavigationManager.push(MovieDetailsScreen(movieId: movie.id));
-          },
-          child: SizedBox(
-            width: cardWidth,
-            child: Padding(
-              padding: EdgeInsets.only(right: isSmallScreen ? 12 : 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  AspectRatio(
-                    aspectRatio: 2 / 3,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(16),
-                      child: Stack(
-                        fit: StackFit.expand,
-                        children: [
-                          Image.network(
-                            movie.posterUrl,
-                            fit: BoxFit.cover,
-                            errorBuilder: (_, __, ___) => Container(
-                              color: Colors.grey[800],
-                              child: const Icon(
-                                Icons.broken_image,
-                                color: Colors.white,
-                                size: 40,
-                              ),
-                            ),
+    return SizedBox(
+      width: cardWidth,
+      child: GestureDetector(
+        onTap: () {
+          NavigationManager.push(MovieDetailsScreen(movieId: movie.id));
+        },
+        child: Padding(
+          padding: EdgeInsets.only(right: isSmallScreen ? 12 : 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize:
+                MainAxisSize.min,
+            children: [
+              AspectRatio(
+                aspectRatio: 2 / 3,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      Image.network(
+                        movie.posterUrl,
+                        fit: BoxFit.cover,
+                        errorBuilder: (_, __, ___) => Container(
+                          color: Colors.grey[850],
+                          child: const Icon(
+                            Icons.broken_image,
+                            color: Colors.white70,
+                            size: 48,
                           ),
-
-                          Positioned(
-                            top: 8,
-                            right: 8,
-                            child: Container(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 8,
-                                vertical: 4,
-                              ),
-                              decoration: BoxDecoration(
-                                color: Colors.black.withOpacity(0.6),
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                              child: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  const Icon(
-                                    Icons.star_border,
-                                    color: Colors.amber,
-                                    size: 14,
-                                  ),
-                                  const SizedBox(width: 4),
-                                  Text(
-                                    "8.5",
-                                    style: AppTextStyles.buttonSmallSemiBold,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
+                        ),
                       ),
-                    ),
+                      Positioned(
+                        top: 10,
+                        right: 10,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 8,
+                            vertical: 4,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.black.withOpacity(0.65),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const Icon(
+                                Icons.star_border,
+                                color: Colors.amber,
+                                size: 16,
+                              ),
+                              const SizedBox(width: 4),
+                              Text(
+                                "8.5",
+                                style:
+                                    AppTextStyles.buttonSmallSemiBold?.copyWith(
+                                      fontSize: 13,
+                                    ) ??
+                                    const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 13,
+                                    ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-
-                  const SizedBox(height: 8),
-
-                  Text(
-                    movie.title,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: AppTextStyles.movieTitle,
-                  ),
-
-                  const SizedBox(height: 4),
-
-                  Text(
-                    movie.genreName,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: AppTextStyles.movieInfo,
-                  ),
-                ],
+                ),
               ),
-            ),
+
+              const SizedBox(height: 8),
+
+              Text(
+                movie.title,
+                maxLines: 1, 
+                overflow: TextOverflow.ellipsis,
+                style: AppTextStyles.movieTitle,
+              ),
+
+              const SizedBox(height: 4),
+
+              Text(
+                movie.genreName,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: AppTextStyles.movieInfo,
+              ),
+
+            ],
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
